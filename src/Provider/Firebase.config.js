da717058bf631d948f239f918851f7947c861ada
sami5671 <samialam5671@gmail.com>
2024-01-25 15:18:28 +0600

    added the authcontext of google,  user,

---
+++ src/Provider/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBS3dENdl71NcAAyuMx2nrviGzzTGS_lxg",
+  authDomain: "cis-club.firebaseapp.com",
+  projectId: "cis-club",
+  storageBucket: "cis-club.appspot.com",
+  messagingSenderId: "815191137936",
+  appId: "1:815191137936:web:baff3c2c417b396ca30215",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
